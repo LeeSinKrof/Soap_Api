75a9b9a7c0bbcab92be56133ebf1e0088f14658f
LeeSinKrof <ceylan.emre@laposte.net>
2024-03-13 15:03:50 +0100

    add cors

--- TravelTimeService.py
+++ TravelTimeService.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://emre-projet802.pages.dev"}})
+CORS(app)
 
 class TravelTimeService(ServiceBase):
     @rpc(float, float, float, _returns=float)
