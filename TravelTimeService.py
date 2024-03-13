b50c470ee603c1d7042c645344d5ec474165b1a3
LeeSinKrof <ceylan.emre@laposte.net>
2024-03-13 21:42:53 +0100

    Update TravelTimeService.py

--- TravelTimeService.py
+++ TravelTimeService.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://emre-projet802.pages.dev", "methods": ["POST", "GET", "OPTIONS"], "headers": ["Content-Type"]}})
+CORS(app, resources={r"/*": {"origins": "*", "methods": "*", "headers": "*"}})
 
 class TravelTimeService(ServiceBase):
     @rpc(float, float, float, _returns=float, _body_style='wrapped')
@@ -28,21 +28,19 @@ application = Application([TravelTimeService], 'travel',
 def soap_service():
     if request.method == 'OPTIONS':
         response_headers = {
-            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': 'https://emre-projet802.pages.dev',
             'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type',
         }
         return '', 200, response_headers
     elif request.method == 'POST':
-        response = wsgi_application.__call__(request.environ, response_headers)
-        return response
+        return WsgiApplication(application)
     else:
         return 'Hello World!'
 
 
 if __name__ == '__main__':
     wsgi_application = WsgiApplication(application)
-    app.wsgi_app = wsgi_application
 
     host = (os.getenv('HOST') or "127.0.0.1")
     port = (int(os.getenv('PORT')) or 8000)
