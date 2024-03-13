1babb7c295f1f5959cbc08187a06720959f560e9
LeeSinKrof <ceylan.emre@laposte.net>
2024-03-13 15:42:18 +0100

    Update TravelTimeService.py

--- TravelTimeService.py
+++ TravelTimeService.py
@@ -2,30 +2,26 @@ from spyne import Application, rpc, ServiceBase
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 from wsgiref.simple_server import make_server
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
-
-
 load_dotenv()
-
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://emre-projet802.pages.dev", "methods": ["GET", "POST", "OPTIONS"], "headers": ["Content-Type"]}})
 
 class TravelTimeService(ServiceBase):
-    @rpc(float, float, float, _returns=float, _body_style='wrapped', _http_method='POST')
+    @rpc(float, float, float, _returns=float, _body_style='wrapped')
     def calculate_travel_time(ctx, distance, autonomy, charging_time):
         return distance / autonomy * charging_time
-
 
 application = Application([TravelTimeService], 'travel',
                           in_protocol=Soap11(validator='lxml'),
                           out_protocol=Soap11())
 
-@app.route('/get', methods=['POST', 'GET', 'OPTIONS'])
+@app.route('/', methods=['POST', 'GET', 'OPTIONS'])
 def soap_service():
     if request.method == 'OPTIONS':
         response_headers = {
@@ -34,6 +30,8 @@ def soap_service():
             'Access-Control-Allow-Headers': 'Content-Type',
         }
         return '', 200, response_headers
+    elif request.method == 'GET':
+        return 'Hello World!'  # Add this line to return 'Hello World!' for GET requests
     else:
         return WsgiApplication(application)
 
